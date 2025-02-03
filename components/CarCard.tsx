3a94f4f1c7e3c40856e07c406a85641497a5c440
CodewithMK180105 <manishkumar180105@gmail.com>
2025-02-03 19:47:30 +0530

    Done with Car Details Box, and displayed using the dialog box of the Shadcn UI Component.

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,100 +1,99 @@
-'use client'
-import { CarProps } from '@/types'
-import { estimateCarPrice, estimateDailyRentalPrice } from '@/utils';
-import Image from 'next/image';
-import React, { useState } from 'react'
-import CustomButton from './CustomButton';
-import CarDetails from './CarDetails';
+"use client";
+import { CarProps } from "@/types";
+import { estimateDailyRentalPrice } from "@/utils";
+import Image from "next/image";
+import CustomButton from "./CustomButton";
+import CarDetails from "./CarDetails";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
-interface CarCardProps{
-    car: CarProps
+interface CarCardProps {
+  car: CarProps;
 }
 
-const CarCard = ({car}: CarCardProps) => {
+const CarCard = ({ car }: CarCardProps) => {
+  const { city_mpg, year, make, model, transmission, drive } = car;
 
-    const { city_mpg, year, make, model, transmission, drive }= car;
+  const carPrice = estimateDailyRentalPrice(car);
 
-    const [isOpen,setIsOpen]=useState(false);
+  return (
+    <div className="car-card group">
+      <div className="car-card__component">
+        <h2 className="car-card__content-title">
+          {make} {model}
+        </h2>
+      </div>
 
-    const carPrice=estimateDailyRentalPrice(car);
+      <p className="flex mt-6 text-[32px] font-extrabold">
+        <span className="self-start text-[14px] font-semibold">$</span>
+        {carPrice}
+        <span className="self-end text-[14px] font-medium">/day</span>
+      </p>
 
-    return (
-        <div className='car-card group'>
-            <div className='car-card__component'>
-                <h2 className='car-card__content-title'>{make} {model}</h2>
-            </div>
+      <div className="relative w-full h-40 my-3 object-contain">
+        <Image
+          src="/hero.png"
+          alt="car model"
+          fill
+          priority
+          className="object-contain"
+        />
+      </div>
 
-            <p className='flex mt-6 text-[32px] font-extrabold'>
-                <span className='self-start text-[14px] font-semibold'>
-                    $
-                </span>
-                    {carPrice}
-                <span className='self-end text-[14px] font-medium'>
-                    /day
-                </span>
+      <div className="relative flex w-full mt-2">
+        <div className="flex group-hover:invisible w-full justify-between text-gray">
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image
+              src="/steering-wheel.svg"
+              alt="steering wheel"
+              width={20}
+              height={20}
+            />
+            <p className="text-[14px]">
+              {transmission === "a" ? "Automatic" : "Manual"}
             </p>
-
-            <div className='relative w-full h-40 my-3 object-contain'>
-                <Image
-                    src="/hero.png"
-                    alt="car model"
-                    fill
-                    priority
-                    className='object-contain'
-                />
-            </div>
-
-            <div className="relative flex w-full mt-2">
-                <div className='flex group-hover:invisible w-full justify-between text-gray'>
-                    <div className='flex flex-col justify-center items-center gap-2'>
-                        <Image
-                            src="/steering-wheel.svg"
-                            alt="steering wheel"
-                            width={20}
-                            height={20}
-                        />
-                        <p className='text-[14px]'>
-                            {transmission==='a'?"Automatic":"Manual"}
-                        </p>
-                    </div>
-                    <div className='flex flex-col justify-center items-center gap-2'>
-                        <Image
-                            src="/tire.svg"
-                            alt="tire"
-                            width={20}
-                            height={20}
-                        />
-                        <p className='text-[14px]'>
-                            {drive.toLocaleUpperCase()}
-                        </p>
-                    </div>
-                    <div className='flex flex-col justify-center items-center gap-2'>
-                        <Image
-                            src="/gas.svg"
-                            alt="Gas"
-                            width={20}
-                            height={20}
-                        />
-                        <p className='text-[14px]'>
-                            {city_mpg} MPG
-                        </p>
-                    </div>
-                </div>
-
-                <div className='car-card__btn-container'>
-                    <CustomButton
-                        title='View More'
-                        containerStyles='w-full py-[16px] rounded-full bg-primary-blue'
-                        textStyles="text-white text-[14px] leading-[17px] font-bold"
-                        rightIcon="/right-arrow.svg"
-                        handleClick={()=>setIsOpen(true)}
-                    />
-                </div>
-            </div>
-
-            <CarDetails isOpen={isOpen} closeModal={()=>setIsOpen(false)} car={car} />
+          </div>
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image src="/tire.svg" alt="tire" width={20} height={20} />
+            <p className="text-[14px]">{drive.toLocaleUpperCase()}</p>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image src="/gas.svg" alt="Gas" width={20} height={20} />
+            <p className="text-[14px]">{city_mpg} MPG</p>
+          </div>
         </div>
-    )
-}
 
-export default CarCard
+        <Dialog>
+          <DialogTrigger asChild>
+            <div className="car-card__btn-container">
+              <CustomButton
+                title="View More"
+                containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
+                textStyles="text-white text-[14px] leading-[17px] font-bold"
+                rightIcon="/right-arrow.svg"
+              />
+            </div>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle></DialogTitle>
+              <DialogDescription>
+                <CarDetails car={car} />
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+      </div>
+    </div>
+  );
+};
+
+export default CarCard;
+
+// Car Details & Rental Information
--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -1,12 +1,126 @@
 import React from 'react'
+import { CarProps } from '@/types'
+import Image from 'next/image';
 
-const CarDetails = () => {
+// interface CarDetails {
+//     isOpen: boolean,
+//     closeModal: ()=>void,
+//     car: CarProps
+// }
+
+interface CarCardProps {
+  car: CarProps;
+}
+
+const CarDetails = ({ car }: CarCardProps) => {
   return (
     <div>
-      CarDetails
-      CarDetails
+
+      <h1 className='font-bold text-xl'>Car Details & Rental Information</h1>
+
+      <div className='mt-4 bg-blue-700 p-4 rounded-xl'>
+        <Image
+          src="/hero.png"
+          alt="car model"
+          width={200}
+          height={200}
+          className="object-contain h-40 w-72 mx-auto"
+        />
+      </div>
+
+      <div className='w-full mt-2 flex justify-between'>
+        <div className='bg-blue-100 p-4 w-[32%] rounded-xl'>
+          <Image
+            src="/hero.png"
+            alt="car model"
+            width={200}
+            height={200}
+            className="object-contain h-16 w-full mx-auto"
+          />
+        </div>
+        <div className='bg-blue-100 p-4 w-[32%] rounded-xl'>
+          <Image
+            src="/hero.png"
+            alt="car model"
+            width={200}
+            height={200}
+            className="object-contain h-16 w-full mx-auto"
+          />
+        </div>
+        <div className='bg-blue-100 p-4 w-[32%] rounded-xl'>
+          <Image
+            src="/hero.png"
+            alt="car model"
+            width={200}
+            height={200}
+            className="object-contain h-16 w-full mx-auto"
+          />
+        </div>
+      </div>
+
+      <h3 className='mt-4 font-semibold text-lg capitalize'>{car.make} {car.model}</h3>
+
+      <div className='mt-1 flex flex-col gap-1'>
+        {Object.entries(car).map(([key, value])=>(
+          <div className='flex justify-between' key={key}>
+            <p className='text-gray-400 capitalize'>{key.split("_").join(" ")}</p>
+            <p className='font-bold'>{value}</p>
+        </div>
+        ))}
+      </div>
+
     </div>
   )
 }
 
 export default CarDetails
+
+
+{/* <div className='flex justify-between'>
+          <p className='text-gray-400'>City Mpg</p>
+          <p className='font-bold'>{car.city_mpg}</p>
+        </div>
+        <div className='flex justify-between'>
+          <p className='text-gray-400'>Class</p>
+          <p className='font-bold'>{car.class}</p>
+        </div>
+        <div className='flex justify-between'>
+          <p className='text-gray-400'>Combination Mpg</p>
+          <p className='font-bold'>{car.combination_mpg}</p>
+        </div>
+        <div className='flex justify-between'>
+          <p className='text-gray-400'>Cylinders</p>
+          <p className='font-bold'>{car.cylinders}</p>
+        </div>
+        <div className='flex justify-between'>
+          <p className='text-gray-400'>Displacement</p>
+          <p className='font-bold'>{car.displacement}</p>
+        </div>
+        <div className='flex justify-between'>
+          <p className='text-gray-400'>Drive</p>
+          <p className='font-bold'>{car.drive}</p>
+        </div>
+        <div className='flex justify-between'>
+          <p className='text-gray-400'>Fuel Type</p>
+          <p className='font-bold'>{car.fuel_type}</p>
+        </div>
+        <div className='flex justify-between'>
+          <p className='text-gray-400'>Highway Mpg</p>
+          <p className='font-bold'>{car.highway_mpg}</p>
+        </div>
+        <div className='flex justify-between'>
+          <p className='text-gray-400'>Make</p>
+          <p className='font-bold'>{car.make}</p>
+        </div>
+        <div className='flex justify-between'>
+          <p className='text-gray-400'>Model</p>
+          <p className='font-bold'>{car.model}</p>
+        </div>
+        <div className='flex justify-between'>
+          <p className='text-gray-400'>Transmission</p>
+          <p className='font-bold'>{car.transmission}</p>
+        </div>
+        <div className='flex justify-between'>
+          <p className='text-gray-400'>Year</p>
+          <p className='font-bold'>{car.year}</p>
+        </div> */}
