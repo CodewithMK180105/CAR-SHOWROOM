0357427b60209a33f832f52da3021c07831feb75
CodewithMK180105 <manishkumar180105@gmail.com>
2025-02-14 19:16:42 +0530

    Filters applied for the Fuel and Year as well.

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,18 +1,48 @@
 'use client'
 import React, { useState } from 'react'
 import SearchFuel from "@/components/SearchFuel"
+import { SearchButton } from './SearchBar';
+import { useRouter } from 'next/navigation';
 
 
 const CustomFilter = () => {
 
   const [fuel, setFuel]=useState('');
+  const router= useRouter();
+
+  const handleSearch=(e: React.MouseEvent<HTMLButtonElement>)=>{
+    e.preventDefault();
+
+    if(fuel===''){
+      return alert('Please select the fuel type');
+    }
+
+    updateSearchParams(fuel.toLowerCase());
+  }
+
+  const updateSearchParams= (fuel: string)=>{
+    const searchParams= new URLSearchParams(window.location.search);
+
+    if(fuel){
+      searchParams.set('fuel',fuel);
+    } else{
+      searchParams.delete('fuel');
+    }
+
+    const newPathName= `${window.location.pathname}?${searchParams.toString()}`;
+
+    router.push(newPathName);
+  }
 
   return (
     <div className="searchbar__item">
       <SearchFuel 
           fuel={fuel}
           setFuel={setFuel}
-        />
+      />
+      <button onClick={handleSearch}>
+        <SearchButton otherClasses="ml-1 rounded-full" />
+      </button>
     </div>
   )
 }
--- components/FilterYear.tsx
+++ components/FilterYear.tsx
@@ -1,18 +1,48 @@
 'use client'
 import React, { useState } from 'react'
 import SearchModelYear  from "@/components/SearchModelYear"
+import { SearchButton } from './SearchBar';
+import { useRouter } from 'next/navigation';
 
 
 const FilterYear = () => {
 
   const [year, setYear]=useState('');
+  const router= useRouter();
+
+  const handleSearch=(e: React.MouseEvent<HTMLButtonElement>)=>{
+      e.preventDefault();
+  
+      if(year===''){
+        return alert('Please select the fuel type');
+      }
+  
+      updateSearchParams(year.toLowerCase());
+    }
+  
+    const updateSearchParams= (year: string)=>{
+      const searchParams= new URLSearchParams(window.location.search);
+  
+      if(year){
+        searchParams.set('year',year);
+      } else{
+        searchParams.delete('year');
+      }
+  
+      const newPathName= `${window.location.pathname}?${searchParams.toString()}`;
+  
+      router.push(newPathName);
+    }
 
   return (
     <div className="searchbar__item">
       <SearchModelYear 
           year={year}
           setYear={setYear}
-        />
+      />
+      <button onClick={handleSearch}>
+        <SearchButton otherClasses="ml-1 rounded-full" />
+      </button>
     </div>
   )
 }
